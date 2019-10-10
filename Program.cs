7216f305e6d9f301d8eb81f1ebf5dc8ef66b4f67
ruchier <ruchier@codev.com>
2019-10-11 06:14:35 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Design
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Singleton sg = Singleton.GetInstance();
+            sg.PrintDetails(sg.GetHashCode().ToString());
+            Singleton sg2 = Singleton.GetInstance();
+            sg.PrintDetails(sg.GetHashCode().ToString());
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Design
+{
+    public sealed class Singleton
+    {
+        public static Singleton instance = null;
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+
+            if (instance == null)
+                instance = new Singleton();
+
+            return instance;
+        }
+
+        public void PrintDetails(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
